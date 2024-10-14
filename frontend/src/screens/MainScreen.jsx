a781bfecb63041d5d42330d2b9f4d836ce8875d1
HarshPatil32 <patilsharsh@gmail.com>
2024-10-14 02:51:15 -0400

    Added some stuff to main screen

--- frontend/src/screens/MainScreen.jsx
+++ frontend/src/screens/MainScreen.jsx
@@ -1,13 +1,25 @@
 import './MainScreen.css';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+
 
 const MainScreen = () => {
 
+    const navigate = useNavigate();
+
+    const handleButton = async () => {
+        navigate('/moving-averages');
+    }
+
+    
     return (
-        <div>
-            {}
-            <h1>Hello</h1>
+        <div className="main-screen">
+            <h1 className="main-title">Trading Bot</h1>
+            <h1 className="strategy-title">Choose Your Strategy</h1>
+            <button onClick = {handleButton} className = "strategy-button"> 
+                Moving Averages 
+            </button>
         </div>
     );
 };
